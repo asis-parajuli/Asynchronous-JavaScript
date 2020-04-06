ec72197cef1c4092ff29509d6768386b0f8bda24
asis-parajuli <60426026+asis-parajuli@users.noreply.github.com>
2020-04-06 12:08:59 +0545

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-console.log("Before");
-setTimeout(() => {
-  console.log("Reading a user from the database ...");
-}, 2000);
-console.log("After");
-
-// here setTimeout defines a asynchronous function as it schedule the task and pass the function to after and then loads after 2 sec
